c39148925d59046a023b27957513b8a9b78dce78
Izabella Melo <cmeloizabella@gmail.com>
2019-03-28 14:43:06 +0000

    use pylint

--- Miniprojeto - Socket Programming/client.py
+++ Miniprojeto - Socket Programming/client.py
@@ -1,5 +1,4 @@
 import socket
-
 import argparse
 
 serverName = "localhost"
@@ -8,12 +7,9 @@ serverPort = 2080
 clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--command', '-c', type=str,
-					help='Command to execute: GET or POST')
-parser.add_argument('--file', '-f', type=str,
-					help='File to read or content to save in file')
-parser.add_argument('--tofile', '-t', type=str,
-					help='File to save')
+parser.add_argument('--command', '-c', type=str, help='Command to execute: GET or POST')
+parser.add_argument('--file', '-f', type=str, help='File to read or content to save in file')
+parser.add_argument('--tofile', '-t', type=str, help='File to save')
 args = parser.parse_args()
 
 print(args.file) # file in
@@ -28,7 +24,7 @@ try:
 	command = args.command
 	contents = args.file
 	toFile = args.tofile
-	
+
 	message = command + "-" + contents
 	clientSocket.send(message.encode()) # send command and contents to server
 	print("I send " + message)
@@ -38,21 +34,20 @@ try:
 		path = "files/" + toFile
 	elif command == "GET":
 		path = toFile
-		
+
 	# save contents in output files
 	with open(path, "wb") as f:
 		data = clientSocket.recv(1024) # receive from server
 		while data:
 			f.write(data)
 			data = clientSocket.recv(1024)
-	
+
 	clientSocket.close()
 
 except KeyboardInterrupt:
 	escape = True
 except Exception:
 	clientSocket.close()
-
 
 clientSocket.close()
 print("\nBye bye :)")
--- Miniprojeto - Socket Programming/server.py
+++ Miniprojeto - Socket Programming/server.py
@@ -1,16 +1,12 @@
 #!/usr/bin/env python3
 
 import sys
-import itertools
 import socket
 from socket import socket as Socket
 
-
-def main():    
-    
+def main():
     # create
     with Socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
-    
         # connect
         server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
@@ -23,18 +19,18 @@ def main():
         while True:
             # connection, client_address = server_socket.accept()
             with server_socket.accept()[0] as connection_socket:
-                print("test")
-                message = connection_socket.recv(1024).decode('ascii') # receive --command and --content
+                # receive --command and --content
+                message = connection_socket.recv(1024).decode('ascii')
                 print("I receive " + message)
-                
+
                 command = message.split('-')[0]
-                
+
                 idx = len(command) + 1
                 request = message[idx:]
-                
+
                 print("Command:" + command)
                 print("Request:" + request)
-                
+
                 if command == "GET":
                     reply = http_handle(request) # read archive and returns its content
                     connection_socket.send(reply) # send message
@@ -45,9 +41,9 @@ def main():
 
     return 0
 
-
 def http_handle(request_string):
-    assert not isinstance(request_string, bytes) # if string "file.txt" isn't a binary
+    # if string "file.txt" isn't a binary
+    assert not isinstance(request_string, bytes)
 
     contents = ''
     with open(request_string, 'rb') as file:
